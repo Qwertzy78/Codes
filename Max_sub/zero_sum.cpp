53095590e209fd7932e8cbf8bf89487704c4cd4b
Naman278 <108816021+Naman278@users.noreply.github.com>
2023-02-07 20:28:26 +0530

    Update zero_sum.cpp

--- Max_sub/zero_sum.cpp
+++ Max_sub/zero_sum.cpp
@@ -1,3 +1,24 @@
+/*
+Given an array of positive and negative integers, develop a linear algorithm and write a C++ code to check if there exist a subarray whose sum is zero is present in it.
+If such a subarray exist then print start and end index of first occurrence. Otherwise print -1 for both start and end index. 
+For example, if the array contains seven elements 10, -3, 2,4, -3, 5, 7 then print start index as 2 and end index as 5
+
+Hint: Use maps for keeping track of sums 
+
+Note: If more than one zero sum subarray exist then print the one which has got maximum length
+
+Input Format
+
+First line contains the number of elements in the array, n
+
+Next line contains the elements separated by a space
+
+Output Format
+
+Either print start index and end index of subarray whose sum becomes 0 or print -1 if no such subarray exist
+*/
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
